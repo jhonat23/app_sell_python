db0f73220c1a36b2d56a4e7bf335901e9cb8e444
jhonat23 <jhonat23@gmail.com>
2022-08-11 11:16:07 -0500

    adding first functions

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 
 clients = 'Pablo, Ricardo'
 
-if __name__ == '__main__':
-    clients += ', David'
+def create_client(client: str):
+    global clients #global is for indicate the use of a global variable into func
+    _add_comma_space()
+    clients += client
+
+def show_clients():
+    global clients
     print(clients)
+
+def _add_comma_space():#private function
+    global clients
+    clients += ', '
+
+if __name__ == '__main__':
+    show_clients()
+    create_client('David')
+    show_clients()
